27ee09a1b3cd29ec61151d201a460b026145e217
NKN1396 <someone.took.nkn1396@gmail.com>
2019-02-17 02:16:10 +0100

    color now gets assigned after new event

--- src/modules/prism2.js
+++ src/modules/prism2.js
@@ -1,18 +1,26 @@
-var colors = require("./../resources/colors.json");
-var { sample } = require("lodash");
+var { sample } = require("lodash")
+
+const makeshift = require("./../resources/makeshift.json")
 
 /**
  * Assigns a random color 5 minutes after joining the Makeshift guild.
- * @param {*} bot The bot client.
+ * @param {*} client The bot client.
  */
-module.exports = function(bot){
-	bot.on("guildMemberAdd", (guildMember)=>{
-		guildMember.changeColorSoon = true;
-		setTimeout(function(){
-			if(guildMember.changeColorSoon){
-				guildMember.changeColorSoon = false;
-				guildMember.addRole(sample(colors));
+module.exports = function(client){
+	client.on("guildMemberGreeted", guildMember => {
+
+		//Check if member joined Makeshift guild
+		if(guildMember.guild.id != makeshift.guild) return
+
+		(async () => {
+			try {
+				if(process.env.DEBUG) console.debug("PRISM 00")
+				await guildMember.addRole(sample(makeshift.roles.colors))
+				if(process.env.DEBUG) console.debug("PRISM 01")
+			} catch (e) {
+				console.error(e)
 			}
-		}, 300000);
-	});
-};
+		})()
+
+	})
+}
