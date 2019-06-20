246fed41cd9161db7dd273f0bf45f1c2a8afb68a
NKN1396 <someone.took.nkn1396@gmail.com>
2019-06-20 13:18:24 +0200

    rewrite to Luxon

--- src/commands/resources/time.js
+++ src/commands/resources/time.js
@@ -1,6 +1,5 @@
-var { Command } = require("discord.js-commando");
-var moment = require("moment");	//TODO: Rewrite to Luxon
-var pad = require("pad-left");
+const { Command } = require("discord.js-commando")
+const { DateTime, Interval } = require("luxon")
 
 module.exports = class command extends Command {
 	constructor(client) {
@@ -14,28 +13,37 @@ module.exports = class command extends Command {
 			group: "resources",
 			memberName: "time",
 			description: "Shows Warframe server time and time left until Warframe daily reset."
-		});
+		})
 	}
 
 	async run(msg) {
-		var timeNow = moment().utc();
-		var minutesUntilReset = Math.floor(moment.duration(moment(timeNow).endOf("day").diff(timeNow)) / 60000);
-		var minutesWithoutHours = minutesUntilReset % 60;
-		minutesWithoutHours = 0;
-		return msg.channel.send(
-			`Current time: **${timeNow.format("HH:mm")}** *(UTC+0)*.`, {
-				embed:{
-					fields:[
-						{
-							name:"New sorties in",
-							value:"DE tied this to their regional time instead of a timezone. This is literally a pain to program."
-						},
-						{
-							name:"Daily reset in",
-							value:`${Math.floor(minutesUntilReset/60)}:${pad(minutesWithoutHours,2,"0")}`
-						}
-					]
-				}
-			});
+		//Figure out the right times
+		let time_now = DateTime.utc()
+		let time_reset = time_now.endOf("day")
+		let time_digitalExtremes = DateTime.utc().setZone("America/Toronto")
+		//Checks if sorties have already been updated today (12am in London, Ontario) and sets the desired day to tommorow.
+		if (time_digitalExtremes.hour >= 12) {
+			time_digitalExtremes = time_digitalExtremes.plus({days: 1})
+		}
+		//Set to sortie refresh time.
+		time_digitalExtremes = time_digitalExtremes.set({ hour: 12, minute: 0, second: 0, millisecond: 0 })
+		//Intervals
+		let interval_reset = Interval.fromDateTimes(time_now, time_reset)
+		let interval_sorties = Interval.fromDateTimes(time_now, time_digitalExtremes)
+		let embed = {
+			embed: {
+				fields: [
+					{
+						name: "New sorties in",
+						value: interval_sorties.toDuration().toFormat("h':'mm")
+					},
+					{
+						name: "Daily reset in",
+						value: interval_reset.toDuration().toFormat("h':'mm")
+					}
+				]
+			}
+		}
+		return msg.channel.send(`Current time: **${time_now.toFormat("h':'mm")}** *(UTC+0)*.`, embed) //TIME_24_SIMPLE
 	}
-};
+}
