b3f55df5bbf5e77f3d5140fb553fc477655b56f4
NKN1396 <someone.took.nkn1396@gmail.com>
2019-07-26 00:35:08 +0200

    Message now displays selection number

--- src/utils/selectRandomly.js
+++ src/utils/selectRandomly.js
@@ -6,7 +6,31 @@ var { sample } = require("lodash")
  * @param {Number} choice The index of the array to return from.
  * @return {Object} The object returned from the specified options.
  */
-module.exports = function (options, choice){
+function random (options, choice) {
 	let option = options[choice - 1]
 	return option?option:sample(options)
 }
+
+/**
+ * Like random, but attaches the selected choice as the message content.
+ * @param {Object[]} options An array of options of which one will be returned.
+ * @param {Number} choice The index of the array to return from.
+ * @param {string} name The name.
+ * @return {Object} The object returned from the specified options.
+ */
+function random2 (options, choice = undefined, name = "Choice") {
+	var option = random(options, choice)
+	option = random(options, choice)
+	var index = options.indexOf(option)
+	if(options.length > 1){
+		let header = `${name} ${index + 1}/${options.length}`
+		if (!option.content) {
+			option.content = header
+		} else {
+			option.content = `${header}\n:${option.content}`
+		}
+	}
+	return option
+}
+
+module.exports = random2
