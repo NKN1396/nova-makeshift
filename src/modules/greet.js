f0b88e842411b4ad5e065cbb25f50bf3512e702a
NKN1396 <someone.took.nkn1396@gmail.com>
2019-07-27 01:44:40 +0200

    equality -> identity

--- src/modules/greet.js
+++ src/modules/greet.js
@@ -8,7 +8,7 @@ module.exports = function(client){
 	client.on("guildMemberAdd", async guildMember => {
 
 		//Check if member joined Makeshift guild
-		if(guildMember.guild.id != makeshift.guild) return
+		if(guildMember.guild.id !== makeshift.guild) return
 
 		await client.channels.get(makeshift.channels.text.general).send(`Welcome to the Makeshift clan Discord, ${guildMember.user}!`)
 			.catch(console.error)
