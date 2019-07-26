1b2f519de09f0aca243c56b019cf27c0b26e6278
NKN1396 <someone.took.nkn1396@gmail.com>
2019-07-26 15:14:38 +0200

    Added function for select&send

--- src/utils/novaCommand.js
+++ src/utils/novaCommand.js
@@ -1,9 +1,19 @@
 const Commando = require("discord.js-commando")
+const select = require("./selectRandomly")
 
 module.exports = class Command extends Commando.Command {
 	constructor(client, info) {
 		info.memberName = info.name
 		if (info.description === undefined) info.description = ""
 		super(client, info)
+	}
+
+	async sendSelect (message, options, args, name) {
+		try {
+			await message.channel.send(select(options, args, name))
+			message.react("✅")
+		} catch (error) {
+			console.error(error)
+		}
 	}
 }
